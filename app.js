0ec7658682419c640efaa029277609e6e98b8e7f
fuzzylogicxx <laurence@fuzzylogicdigital.com>
2019-09-15 21:49:47 +0100

    Messing around with the callback pattern

--- app.js
+++ app.js
@@ -16,17 +16,29 @@ const request = require('request')
 
 // MapBox (geocoding)
 
-const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZnV6enlsb2dpYyIsImEiOiJjazBsZDgyeDUwc2k5M2RwYmhodWxoZHE2In0.w1CGHgEJOzuK88BGkB5R-w&limit=1"
+// const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZnV6enlsb2dpYyIsImEiOiJjazBsZDgyeDUwc2k5M2RwYmhodWxoZHE2In0.w1CGHgEJOzuK88BGkB5R-w&limit=1"
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to geocoding service')
-    } else if (response.body.features.length === 0) {
-        console.log('Unable to find location')
-    } else {
-        const longitude = response.body.features[0].center[0]
-        const latitude = response.body.features[0].center[1]
-        console.log(longitude, ',', latitude)
-    }
+// request({ url: geocodeURL, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to geocoding service')
+//     } else if (response.body.features.length === 0) {
+//         console.log('Unable to find location')
+//     } else {
+//         const longitude = response.body.features[0].center[0]
+//         const latitude = response.body.features[0].center[1]
+//         console.log(longitude, ',', latitude)
+//     }
+// })
+
+const add = (first, second, callback) => {
+    setTimeout(() => {
+        const sum = first + second
+
+        callback(sum)
+    }, 2000);
+} 
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
 })
 
