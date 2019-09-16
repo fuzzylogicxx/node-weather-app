9cb907b78e185104dbc76a4ede09a80701373033
fuzzylogicxx <laurence@fuzzylogicdigital.com>
2019-09-16 20:17:04 +0100

    Added partials for regions which are repeated, e.g. Header

--- src/app.js
+++ src/app.js
@@ -1,16 +1,41 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../views')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
+// Sert up handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Set up directory to serve
 app.use(express.static(publicDirectoryPath))
 
+// Routes
 app.get('', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'Weather App', 
+        name: 'Laurence Hughes'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About', 
+        name: 'Laurence Hughes'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help', 
+        name: 'Laurence Hughes', 
+        helpMsg: 'Help me Obi-wan'
+    })
 })
 
 app.get('/weather', (req, res) => {
