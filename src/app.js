bc029f596a8e699a0e1046d0f6afea6b1fd43fbe
fuzzylogicxx <laurence@fuzzylogicdigital.com>
2019-09-16 19:40:29 +0100

    Added static assets, introduced handlebars via hbs as templating engine

--- src/app.js
+++ src/app.js
@@ -3,7 +3,15 @@ const express = require('express')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../views')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index')
+})
 
 app.get('/weather', (req, res) => {
     res.send({
