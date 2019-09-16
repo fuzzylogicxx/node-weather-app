2d6d4a49ce3dc108928a52ce893420fb41eecadd
fuzzylogicxx <laurence@fuzzylogicdigital.com>
2019-09-16 12:56:56 +0100

    Accept a location e.g. Glasgow for weather from command line

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Glasgow', (error, data) => {
-    console.log('Error:', error)
-    console.log('Data:', data)
-})
+const userLocation = process.argv[2];
 
-forecast('-75.7088', 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+geocode(userLocation, (error, data) => {
+    if (!userLocation) {
+        return console.log('Please provide a location.')
+    }
+    
+    if (error) {
+        return console.log(error)
+    }
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log(data.location)
+        console.log(forecastData)
+    })
 })
