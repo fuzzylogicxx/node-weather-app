429e4de04abc4884c3058ad0209950eab43c7163
fuzzylogicxx <laurence@fuzzylogicdigital.com>
2019-09-17 19:03:36 +0100

    Added 'get weather for location' form which uses fetch()

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,27 @@
-console.log('Client side JS')
+const weatherForm = document.querySelector('#weather-form')
+const search = document.querySelector('#search')
+const messageOne = document.querySelector('#message-one')
+const messageTwo = document.querySelector('#message-two')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const location = search.value
+    
+    if (location === '') {
+        console.log('Please provide a location')
+    } else {
+        messageOne.textContent = 'Loading…'
+        messageTwo.textContent = ''
+        fetch('http://localhost:3000/weather/?address=' + location).then(response => {
+            response.json().then((data) => {
+                if (data.error) {
+                    messageOne.textContent = data.error
+                } else {
+                    messageOne.textContent = data.location
+                    messageTwo.textContent = data.forecast
+                }
+            })
+        })
+    }
+})
