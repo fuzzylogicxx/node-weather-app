2be8084595e3fc9927957bfa31f1f4a00cb1f1d9
fuzzylogicxx <laurence@fuzzylogicdigital.com>
2019-09-17 17:25:05 +0100

    express takes /weather?address=Glasgow, calls geocode then forecast, returns JSON response

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -39,12 +41,48 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-    res.send({
-            forecast: 'Sunny', 
-            location: 'Glasgow'
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+    
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({
+                error // key and val have same name so use object shorthand syntax
+            })
+        }
+
+        forecast(latitude, longitude, (error, forecastDetail) => {
+            if (error) {
+                return res.send({
+                    error
+                })
+            }
+
+            res.send({
+                forecast: forecastDetail, 
+                address: req.query.address,
+                location
+                
+            })
+        })
     })
 })
 
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
